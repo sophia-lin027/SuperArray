55f27b1e82f04858f9bca07fce541251ca2ff8ac
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-11-01 21:30:43 -0500

    added constructor-- 1a, b

---
+++ SuperArray.java
@@ -0,0 +1,10 @@
+public class SuperArray {
+  private String[] data;
+  private int size;
+
+  public SuperArray() {
+    SuperArray new_array = new SuperArray();
+    size = 0;
+    capacity = new String[10];
+  }
+}
