f323603bd7ba3683b1db4c493aee78b99a7b7fa9
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-11-05 14:02:00 -0500

    change to remove?

--- SuperArray.java
+++ SuperArray.java
@@ -84,15 +84,16 @@ public class SuperArray {
 
   public String remove (int index) {
     String x = data[index];
-    for (int i = index; i < data.length - 1; i++) {
+
+    for (int i = index; i < size(); i++) {
       data[i] = data[i + 1];
     }
-    size++;
+
+    size--;
     return x;
   }
 
   public int indexOf (String s) {
-    int result = 0;
     for (int i = 0; i < size; i++) {
       if (data[i] == s) {
         return i;
