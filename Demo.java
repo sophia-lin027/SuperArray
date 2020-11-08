612cea8597b59d07702f70867b08c152bc8ae3a3
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-11-08 14:13:40 -0500

    added zip

--- Demo.java
+++ Demo.java
@@ -19,6 +19,27 @@ public class Demo{
     return new_array;
   }
 
+  public static SuperArray zip (SuperArray a, SuperArray b) {
+    int maximum = Math.max(a.size(), b.size());
+    int minimum = Math.min(a.size(), b.size());
+    SuperArray new_array = new SuperArray();
+    for (int i = 0; i < minimum; i++) {
+      new_array.add(a.get(i));
+      new_array.add(b.get(i));
+    }
+    if (a.size() != b.size()) {
+      for (int i = minimum; i < maximum; i++) {
+        if (a.size() > b.size()) {
+          new_array.add(a.get(i));
+        }
+        else {
+          new_array.add(b.get(i));
+        }
+      }
+    }
+    return new_array;
+  }
+
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
@@ -27,17 +48,27 @@ public class Demo{
     words.add("una");    words.add("ebi");     words.add("toro");
 
     SuperArray a = new SuperArray();
-    SuperArray b = new SuperArray();
+    // SuperArray b = new SuperArray();
     a.add("9");   a.add("1");     a.add("2");     a.add("2");
-    a.add("3");   a.add("4");
-    b.add("0");   b.add("4");     b.add("2");     b.add("2");
-    b.add("9");   b.add("0");     b.add("4");
+    // a.add("3");   a.add("4");
+    // b.add("0");   b.add("4");     b.add("2");     b.add("2");
+    // b.add("9");   b.add("0");     b.add("4");
 
-    System.out.println(words);
-    System.out.println(words);
-    System.out.println(a);
+    // System.out.println(words);
+    // System.out.println(words);
+    // System.out.println(a);
+    // //System.out.println(b);
+    // //System.out.println(findOverlap(a, b));
+    // System.out.println(words.lastIndexOf("una"));
+
+    SuperArray b = new SuperArray();
+    b.add("2");   b.add("4");     b.add("2");     b.add("0");
+    SuperArray c = new SuperArray();
+    c.add("2");   c.add("4");     c.add("2");     c.add("0");
+    System.out.println(c);
     System.out.println(b);
-    System.out.println(findOverlap(a, b));
-    System.out.println(words.lastIndexOf("una"));
+    System.out.println(c.equals(b));
+    System.out.println(a);
+    System.out.println(zip(a, b));
   }
 }
