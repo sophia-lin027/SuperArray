f652cd1395a6e2650dc29506ed6d6226146cf66e
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-11-11 13:45:33 -0500

    added error handling for set

--- SuperArray.java
+++ SuperArray.java
@@ -31,6 +31,12 @@ public class SuperArray {
   }
 
   public String set (int index, String element) {
+    if (index < 0.0) {
+      throw new IndexOutOfBoundsException("The index " + index + " cannot be negative");
+    }
+    if (index >= size()) {
+      throw new IndexOutOfBoundsException("The index " + index + " must be less than the size");
+    }
     String x = data[index];
     data [index] = element;
     return x;
